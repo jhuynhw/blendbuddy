2d91e5221badfb2256a13c2643b2d8c09504686e
jhuynhw <jhuynhwei@gmail.com>
2023-05-12 21:12:26 -0700

    Initial Commit

---
+++ blendbuddy/blendbuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  blendbuddyApp.swift
+//  blendbuddy
+//
+//  Created by Johnathan Huynh on 5/12/23.
+//
+
+import SwiftUI
+
+@main
+struct blendbuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
